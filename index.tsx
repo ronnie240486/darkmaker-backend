df808a2695f0c992c3e8db186f6e0b33205ffd09
ronnie240486 <ronnie240486@gmail.com>
2026-01-12 23:44:53 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import app from './app';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -11,6 +11,6 @@ if (!rootElement) {
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <app />
   </React.StrictMode>
 );
